0c75627a3db1fb3717c9d551473fc60d53b19135
sono <rhj080471@gmail.com>
2024-08-14 02:45:42 +0900

    Feat : 원시타입과 리터럴 타입 강의 수강

---
+++ section2/src/primitiveType.ts
@@ -0,0 +1,25 @@
+// number
+let num: number = 123;
+let num2: number = Infinity;
+let num3: number = NaN;
+
+// string
+let str: string = "hello";
+let str1: string = "hello";
+let str2: string = `hello ${str}`;
+
+// boolean
+let bool: boolean = true;
+let bool1: boolean = false;
+
+// null
+let n: null = null;
+
+// undefined
+let u: undefined = undefined;
+
+// 리터럴 타입
+let numB: 10 = 10;
+// numB = 20; // '20' 형식은 '10' 형식에 할당할 수 없습니다.
+let strB: "hello" = "hello";
+// strB = "world"; // 'world' 형식은 'hello' 형식에 할당할 수 없습니다.
