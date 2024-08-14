9b448a2ad836de74bad8fec188fe116909132f91
sono <rhj080471@gmail.com>
2024-08-14 17:02:33 +0900

    Feat : 객체 타입 강의 수강

---
+++ section2/src/object.ts
@@ -0,0 +1,16 @@
+// 객체
+let user: {
+  id?: number;
+  name: string;
+} = {
+  id: 1,
+  name: "sono",
+};
+
+let config: {
+  readonly apiKey: string;
+} = {
+  apiKey: "Sfasdfasdf",
+};
+
+// config.apiKey = "sdfa";
